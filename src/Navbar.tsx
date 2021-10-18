8e74a07d1571f6edc09ae184bdf5f674b1f77c78
nacerillo <nicholascerillo@gmail.com>
2021-10-18 18:28:58 -0400

    fixing title

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,7 +3,7 @@ export const Navbar: React.FC = () => {
   return (
       <div className = "navbar bg-primary">
             <h1 className = "text-dark">
-                Welcome To My Today List!
+                Welcome To My Todo List!
             </h1>
         </div>
   );
